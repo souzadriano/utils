f6abf91233a57b4b24de79186f025c848df35943
Adriano de Souza <souzadriano@gmail.com>
2017-07-20 14:47:34 -0300

    Update JavaDoc

--- src/main/java/souzadriano/utils/BeanEmpty.java
+++ src/main/java/souzadriano/utils/BeanEmpty.java
@@ -13,10 +13,26 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * Check if a Bean Object is empty or is blank.
+ * 
+ * @author Adriano de Souza
+ *
+ */
 public class BeanEmpty {
 
 	private static final Set<Class<?>> WRAPPER_TYPES = getWrapperTypes();
 
+	/**
+	 * Check if fields are null or fields of type string are empty (lenght == 0)
+	 * 
+	 * @param bean Bean Object
+	 * @return true if object is empty or null
+	 * @throws IntrospectionException
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
+	 * @throws InvocationTargetException
+	 */
 	public static boolean isEmpty(Object bean) throws IntrospectionException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		if (bean == null) {
 			return true;
@@ -39,18 +55,60 @@ public class BeanEmpty {
 		return true;
 	}
 	
+	/**
+	 * Check if fields are not null and fields of type string are not empty (lenght == 0)
+	 * 
+	 * @param bean Bean Object
+	 * @return true if object is empty or null
+	 * @throws IntrospectionException
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
+	 * @throws InvocationTargetException
+	 */
 	public static boolean isNotEmpty(Object bean) throws IntrospectionException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		return !isEmpty(bean);
 	}
 	
+	/**
+	 * Check if fields are null or fields of type string are blank or fields of type collection are empty
+	 * 
+	 * @param bean Bean Object
+	 * @return true if object is blank or null
+	 * @throws IntrospectionException
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
+	 * @throws InvocationTargetException
+	 */
 	public static boolean isBlank(Object bean) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, IntrospectionException {
 		return isBlank(bean, false);
 	}
 	
+	
+	/**
+	 * Check if fields are not null and fields of type string are not blank and fields of type collection are not empty
+	 * 
+	 * @param bean Bean Object
+	 * @return true if object is blank or null
+	 * @throws IntrospectionException
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
+	 * @throws InvocationTargetException
+	 */
 	public static boolean isNotBlank(Object bean) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, IntrospectionException {
 		return !isBlank(bean);
 	}
 	
+	/**
+	 * Check if fields are null or fields of type string are blank or fields of type collection are empty
+	 * 
+	 * @param bean Bean Object
+	 * @param deep check fields of a bean field
+	 * @return true if object is blank or null
+	 * @throws IntrospectionException
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
+	 * @throws InvocationTargetException
+	 */
 	public static boolean isBlank(Object bean, boolean deep) throws IntrospectionException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		if (bean == null) {
 			return true;
@@ -90,19 +148,49 @@ public class BeanEmpty {
 		return true;
 	}
 
+	/**
+	 * Check if fields are not null and fields of type string are not blank and fields of type collection are not empty
+	 * 
+	 * @param bean Bean Object
+	 * @param deep check fields of a bean field
+	 * @return true if object is blank or null
+	 * @throws IntrospectionException
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
+	 * @throws InvocationTargetException
+	 */
 	public static boolean isNotBlank(Object bean, boolean deep)
 			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, IntrospectionException {
 		return !isBlank(bean, deep);
 	}
 
+	/**
+	 * Checks if a CharSequence is empty (lenght == 0) or null.
+	 * 
+	 * @param chars the CharSequence to check
+	 * @return true if the CharSequence is empty or null
+	 */
 	public static boolean isEmpty(final CharSequence chars) {
 		return chars == null || chars.length() == 0;
 	}
 	
+	/**
+	 * Checks if a CharSequence is not empty (lenght == 0) and not null.
+	 * 
+	 * @param chars the CharSequence to check
+	 * @return true if the CharSequence is not empty and not null
+	 */
 	public static boolean isNotEmpty(final CharSequence chars) {
 		return !isEmpty(chars);
 	}
 
+	
+	/**
+	 * Checks if a CharSequence is empty (lenght == 0), null or whitespace only.
+	 * 
+	 * @param chars the CharSequence to check
+	 * @return true if the CharSequence is null, empty or whitespace only
+	 */
 	public static boolean isBlank(final CharSequence chars) {
 		if (isEmpty(chars)) {
 			return true;
@@ -116,6 +204,12 @@ public class BeanEmpty {
 		return true;
 	}
 	
+	/**
+	 * Checks if a CharSequence is not empty (lenght == 0), not null and not whitespace only.
+	 * 
+	 * @param chars the CharSequence to check
+	 * @return true if the CharSequence is not null, not empty and not whitespace only
+	 */
 	public static boolean isNotBlank(final CharSequence chars) {
 		return !isBlank(chars);
 	}
